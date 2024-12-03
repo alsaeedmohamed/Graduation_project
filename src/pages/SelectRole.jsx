0ffe67d5311b1188cebfc3891885aff743ab5654
alsaeedmohamed <alsaeedmohamed@std.mans.edu.eg>
2024-12-03 02:53:02 +0200

    1.0.2

--- src/pages/SelectRole.jsx
+++ src/pages/SelectRole.jsx
@@ -22,7 +22,7 @@ function RoleSelectionPage() {
       <div className="flex-1  flex items-center justify-center">
         <div className="text-center px-4">
           {/* Image */}
-          <h3 className="text-2xl font-bold text-gray-700 mt-6">
+          <h3 className="text-2xl font-bold text-gray-9 00 mt-6">
           Select Your Role To Continue         </h3>
           <img
             src={Role} 
@@ -37,12 +37,20 @@ function RoleSelectionPage() {
         <div className="flex items-center justify-center min-h-screen  ">
        
         <div className="bg-white rounded-lg shadow-lg p-8 mt-17 mr-9 ml-9"style={{ width: '660px', height: '400px' }}>
-        <h1 className="text-2xl font-bold mb-4 text-center">Select your Role</h1>
-        <p className="text-gray-500 text-center mb-6">
+        <h1 className="text-3xl font-bold mb-4 text-center">Select your Role</h1>
+        <p className="text-gray-600 text-center mb-6">
           Lorem ipsum dolor sit amet consectetur. Sed nulla tellus
         </p>
         <form>
+          {/* Radio Buttons */}
+          <div className="mb-2">
+          <label className="block font-medium text-gray-800 text-left">
+           <h2> Select your Role : </h2>
+          </label>
+          </div>
+          {/* doctor */}
           <div className="flex flex-col mb-4">
+            <div className="w-full mt-1 mb-1 p-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 hover:ring-[#0C7489] text-gray-900 text-left">
             <label className="flex items-center mb-2">
               <input
                 type="radio"
@@ -51,29 +59,41 @@ function RoleSelectionPage() {
                 className="mr-2"
                 onChange={handleRoleChange}
               />
-              Doctor
+              <p > Doctor </p>
             </label>
+            </div>
+          {/* patient */}
+            <div className="flex flex-col mb-4">
+            <div className="w-full mt-1 mb-1 p-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 hover:ring-[#0C7489] text-gray-900 text-left">
             <label className="flex items-center mb-2">
               <input
                 type="radio"
                 name="role"
-                value="patient"
+                value="doctor"
                 className="mr-2"
                 onChange={handleRoleChange}
               />
-              Patient
+              <p > patient </p>
             </label>
-            <label className="flex items-center">
+            </div>
+
+          {/* Amenities */}
+          <div className="flex flex-col mb-4">
+            <div className="w-full mt-1 mb-1 p-1 border border-gray-300 rounded-md focus:outline-none focus:ring-2 hover:ring-[#0C7489] text-gray-900 text-left">
+            <label className="flex items-center mb-2">
               <input
                 type="radio"
                 name="role"
-                value="amenities"
+                value="doctor"
                 className="mr-2"
                 onChange={handleRoleChange}
               />
-              Amenities
+              <p > Amenities </p>
             </label>
+            </div>
           </div>
+          
+          {/* Continue Button */}
           <Link
             to={getLinkPath()}
             className={`w-full block text-center bg-[#0C7489] text-white py-2 px-4 rounded ${
@@ -82,6 +102,8 @@ function RoleSelectionPage() {
           >
             Continue
           </Link>
+          </div>
+          </div>
         </form>
       </div>
     </div>
@@ -89,6 +111,6 @@ function RoleSelectionPage() {
   );
 }
 
- {/* <span  className="w-full bg-[#0C7489] text-white py-2 px-4 rounded-lg hover:bg-[#065a67] transition duration-300"  style={{ width: '560px', height: '55px', paddingLeft:'270px',paddingRight:'265px' }}>  <Link to="/src/pages/SignUpForm.jsx">Continue</Link></span> */}
+
 
 export default RoleSelectionPage;
